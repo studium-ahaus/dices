41265db8129a76ccc76427c8ba93aec7fd529761
ben <b.pesch@protonmail.com>
2020-05-05 23:03:33 +0200

    :art: Cleanup code

--- src/Plotter.py
+++ src/Plotter.py
@@ -1,16 +1,12 @@
+# zu testzwecken noch vorhanden
+import random
 import shutil
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-# zu testzwecken noch vorhanden
-import random
-
 
 class Plotter:
-    def __init__(self):
-        pass
-
-    # Diagramm in der Konsole erstellen
     def plot(self, diceList):
         diceList = self.__convertList(diceList)
         totalThrows = sum(diceList)
@@ -22,33 +18,34 @@ class Plotter:
         print("Visual representation:")
         print("=" * 100)
 
-        for i in range(1, 7):
-            percentage = 100 * diceList[i - 1] / totalThrows
-            bar = "#" * int(100 * diceList[i - 1] / totalThrows)
+        for j in range(1, 7):
+            percentage = 100 * diceList[j - 1] / totalThrows
+            bar = "#" * int(100 * diceList[j - 1] / totalThrows)
 
-            print("] {} - {: >5}%: \t".format(i, percentage), bar)
+            print("] {} - {: >5}%: \t".format(j, percentage), bar)
 
         print("=" * 100)
 
     # Diagramm mit Matplotlib erstellen
-    def plotAsDiagram(self, dicelist, average, deviation, export=False):
-        lst = np.arange(0, len(dicelist[0])*6, 0.001)
-        gauss = self.__calculateGaussCurve(lst, average, deviation)
-        
+    def plotAsDiagram(self, diceList, avg, dev, export=False):
+        arrangedList = np.arange(0, len(diceList[0]) * 6, 0.001)
+        gauss = self.__calculateGaussCurve(arrangedList, avg, dev)
+
         plt.xlabel("Eye-sum")
         plt.ylabel("Probability")
         plt.grid(True)
+
         colors = ['r', 'c', 'b', 'g']
         colors += list(reversed(colors))
 
-        for i, color in zip(range(-4, 4), colors):
-            plt.fill_between(lst , gauss, 
-             where=np.logical_and(lst <= average - (i * deviation), 
-             lst >= average - ((i + 1) * deviation)), facecolor=color, alpha=0.5)
+        for j, color in zip(range(-4, 4), colors):
+            plt.fill_between(arrangedList, gauss,
+                             where=np.logical_and(arrangedList <= avg - (j * dev),
+                                                  arrangedList >= avg - ((j + 1) * dev)), facecolor=color, alpha=0.5)
 
-        plt.plot(range(1, len(dicelist[0]) * 6 + 1),
-         [100*i/len(dicelist) for i in self.__convertListForDiagram(dicelist)],
-         'ro', lst, gauss)
+        plt.plot(range(1, len(diceList[0]) * 6 + 1),
+                 [100 * j / len(diceList) for j in self.__convertListForDiagram(diceList)],
+                 'ro', arrangedList, gauss)
 
         if export:
             plt.savefig("plot")
@@ -58,29 +55,35 @@ class Plotter:
     # Augenzahlen summieren und in einer Liste zurueckgeben
     def __convertListForDiagram(self, diceList):
         tmpList = []
-        for i in diceList:
-            tmpList.append(sum(i))
-        outList = [0]*len(diceList[0])*6
-        
-        for i in tmpList:
-            outList[i - 1] += 1
-        
+
+        for j in diceList:
+            tmpList.append(sum(j))
+
+        outList = [0] * len(diceList[0]) * 6
+
+        for j in tmpList:
+            outList[j - 1] += 1
+
         return outList
 
     # Haeufigkeit von augenzahlen bestimmen und in einer Liste zurueckgeben
     def __convertList(self, diceList):
         numList = [0] * 6
-        for i in range(7):
-            numList[i - 1] = diceList.count(i)
+
+        for j in range(7):
+            numList[j - 1] = diceList.count(j)
 
         return numList
 
     # Gauss-Kurven-Werte an den Stellen in interator berechnen und zurueckgeben
     def __calculateGaussCurve(self, iterator, mu, sigma):
-        lst = [0]* len(iterator)
-        for i in range(1, len(iterator) + 1):
-            lst[i-1] = 100 * ( (1/(sigma * np.sqrt(2 * np.pi)))*np.exp(-((iterator[i-1]-mu)/sigma)**2/2) )
-        return lst
+        result = [0] * len(iterator)
+
+        for j in range(1, len(iterator) + 1):
+            result[j - 1] = 100 * (
+                    (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-((iterator[j - 1] - mu) / sigma) ** 2 / 2))
+
+        return result
 
 
 # zu testzwecken noch vorhanden
@@ -91,14 +94,14 @@ throwCount = 100000
 for i in range(throwCount):
     tmp = []
     for _ in range(6):
-        tmp.append(random.randint(1,6))
+        tmp.append(random.randint(1, 6))
     lst.append(tmp)
 
 # Mittelwert bestimmen
-average = sum([sum(item) for item in lst])/len(lst)
+average = sum([sum(item) for item in lst]) / len(lst)
 
 # Standardabweichung bestimmen
-deviation = np.sqrt(sum([(sum(item)-average)**2 for item in lst])/(throwCount-1))
+deviation = np.sqrt(sum([(sum(item) - average) ** 2 for item in lst]) / (throwCount - 1))
 
 # als Diagramm mit matplotlib plotten
 p.plotAsDiagram(lst, average, deviation, True)
